a2a1a09ec68057453f1b3a1abb5cb509059f1709
Dolores <lolacanepa@gmail.com>
2022-11-18 17:38:15 -0300

    comentario

--- src/components/styles.js
+++ src/components/styles.js
@@ -14,27 +14,6 @@ export const styles = StyleSheet.create({
       marginBottom: 10,
       color: '#2e2e1f',
     }, 
-    listItemContainer: {
-      paddingVertical: 20,
-      backgroundColor: '#adad85',
-      borderRadius: 5,
-      shadowColor: "#000",
-      shadowOffset: {
-	      width: 0,
-	      height: 2,
-    },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-
-      elevation: 5,
-      justifyContent: 'center',
-      marginVertical: 5,
-    },
-    listItem: {
-      fontSize: 15,
-      color: '#000000',
-      paddingHorizontal: 10, 
-    },
     modalContainer: {
        justifyContent: 'center',
        alignItems: 'center',
---
+++ src/components/task/item/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create ({
+    listItemContainer: {
+        paddingVertical: 20,
+        backgroundColor: '#adad85',
+        borderRadius: 5,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+      },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+  
+        elevation: 5,
+        justifyContent: 'center',
+        marginVertical: 5,
+      },
+      listItem: {
+        fontSize: 15,
+        color: '#000000',
+        paddingHorizontal: 10, 
+      },
+});
